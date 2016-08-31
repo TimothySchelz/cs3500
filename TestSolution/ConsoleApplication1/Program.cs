95fe25f0174ed85a7f62b77c4bb9bc684d152385
Schelz <Schelz@lab7-14>
2016-08-31 12:25:12 -0600

    Added a new message

---
+++ TestSolution/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kill all humans");
+
+            Console.ReadKey();
+        }
+    }
+}
