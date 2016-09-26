280ba9c72adb419bd500eded000a71760e63ca10
TimothySchelz <Timothyschelz@gmail.com>
2016-09-25 20:20:38 -0600

    Created a helper Cell class.  They represent the cells of the spreadsheet.  Should hopefully make things a bit easier.

--- PS4/Resources/README.txt
+++ PS4/Resources/README.txt
@@ -9,7 +9,8 @@ There will be a helper class called Cell that has a name, contents, and a value.
 The contents will be the String or the Formula that the cell contains.  The value will be either the String in the contents
 or it will be whatever the Formula evaluates to.  Each cell will contain either a string or a Formula object.  For constants 
 such as "5" the formula will just be a formula with that value in it. This way we don't have to distinguish between Formulas 
-and doubles when doing things with the cells.
+and doubles when doing things with the cells.  The Cell class has been designed with some things that will probably come in 
+handy later in PS5.
 
 The naming scheme of the tests is Public_NameofMethodBeingTested_SpecificCase().  So if I wanted to test GetCellContents on a 
 cell that is empty it would be called Public_GetCellContents_EmptyCell().  Numbers can also follow the specific case if I have
--- PS4/Spreadsheet/Spreadsheet.cs
+++ PS4/Spreadsheet/Spreadsheet.cs
@@ -11,6 +11,7 @@ namespace SS
 {
     public class Spreadsheet : AbstractSpreadsheet
     {
+        private Dictionary<String, Cell> cells = new Dictionary<String, Cell>();
         /// <summary>
         /// A public constructor to create a new Spreadsheet object.  It just creates an empty 
         /// spreadsheet.  It should not have any filled cells.  Nothing special
@@ -48,6 +49,105 @@ namespace SS
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// A class to act as one cell in a spreadsheet.  It can hold either a String or a Formula.
+    /// Let's try to make it immutable.  This way when it gets replaced by something it just 
+    /// makes a new one.
+    /// </summary>
+    internal class Cell
+    {
+        // This cell holds either a String or a Formula
+        private String StringContent;
+        private Formula FormContent;
+
+        // The lookup delegate to be used
+        Func<string, Double> variableFinder;
+        
+        /// <summary>
+        /// The type of cell it is.  It will be "String" if there is a string in the cell
+        /// It will be "Formula" if there is a Formula in the cell.
+        /// </summary>
+        internal String Type
+        {
+            get;
+
+            private set;
+        }
+
+        /// <summary>
+        /// Constructor to build a Cell from a String
+        /// </summary>
+        /// <param name="s">The string to go in the cell</param>
+        internal Cell(String s)
+        {
+            Type = "String";
+            StringContent = s;
+        }
+
+        /// <summary>
+        /// Constructor to build a Cell from a Formula
+        /// </summary>
+        /// <param name="f"></param>
+        /// <param name="l">The delegate used to look up varaibles.  Takes a string and
+        /// returns a double</param>
+        internal Cell(Formula f, Func<string, Double> l)
+        {
+            Type = "Formula";
+            FormContent = f;
+            variableFinder = l;
+        }
+
+        /// <summary>
+        /// Constructor to build a Cell from a Double
+        /// </summary>
+        /// <param name="d">The double to be put into a cell</param>
+        internal Cell(double d)
+        {
+            Type = "Formula";
+            FormContent = new Formula("" + d);
+        }
+
+        /// <summary>
+        /// A no argument Constructor
+        /// </summary>
+        internal Cell()
+        {
+            Type = "String";
+            StringContent = "";
+        }
+
+        /// <summary>
+        /// Get the content of the Cell
+        /// </summary>
+        /// <returns>The Content of the cell</returns>
+        internal object getContents()
+        {
+            if (Type == "String")
+            {
+                return StringContent;
+            } else
+            {
+                return FormContent;
+            }
+        }
+
+        /// <summary>
+        /// Get the value of the Cell
+        /// </summary>
+        /// <returns>The value of the cell</returns>
+        internal object getValue()
+        {
+            if (Type == "String")
+            {
+                return StringContent;
+            }
+            else
+            {
+                return FormContent.Evaluate(variableFinder);
+            }
         }
     }
 }
