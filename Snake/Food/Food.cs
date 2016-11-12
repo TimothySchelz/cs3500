3833d9fe9404fa317e0b8cea5cd11830f7cfd8d5
Gray Marchese <marchesegray@gmail.com>
2016-11-11 22:19:44 -0700

    Started project, created Snake, Food, and World Classes. 11/11

---
+++ Snake/Food/Food.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using World;
+
+namespace Food
+{
+    public class Food
+    {
+        /// <summary>
+        /// ID for this peice of food.
+        /// </summary>
+        public int ID
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// X and Y coordinate point for the food's location. When food is eaten,
+        /// the point is (-1,-1).
+        /// </summary>
+        public Point loc
+        {
+            get;
+            private set;
+        }
+
+        public Food( int ID, Point loc)
+        {
+            this.ID = ID;
+            this.loc = loc;
+        }
+       
+    }
+}
---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using World;
+
+namespace Snake
+{
+    public class Snake
+    {
+        //Head, turning points, and tail of the snake.
+        private LinkedList<Point> Verticies;
+
+        /// <summary>
+        /// ID of the snake.
+        /// </summary>
+        public int ID
+        {
+            get;
+            private set;
+        }
+
+        //Name of snake as decided by associated player.
+        private string Name;
+
+        public Snake (LinkedList<Point> Verticies, int ID, string Name)
+        {
+            this.Verticies = Verticies;
+            this.ID = ID;
+            this.Name = Name;
+        }
+
+    }
+}
